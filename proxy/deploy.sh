728e41aee4845d73a860dd861ed3cce41ff01dd5
David H <2668621+dhet@users.noreply.github.com>
2020-09-23 09:23:04 +0200

    Remove manual deploy script

--- proxy/deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-deploy_dir=/root/proxy
-
-ssh root@davidhettler.net mkdir -p $deploy_dir
-scp -r * root@davidhettler.net:$deploy_dir
-ssh root@davidhettler.net docker-compose -p proxy -f $deploy_dir/docker-compose.yml up -d
-
-
